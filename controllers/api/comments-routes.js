a4d93916b56c3dc07c7fa520f2aff1b1c1106acb
muktar ahmed <muktar.ahmed13@yahoo.com>
2022-01-22 14:14:41 -0600

    pushing to heroku

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../../models/Association");
-const { findAll } = require("../../models/user");
 
 router.get('/', (req, res)=>{
     Comment.findAll({
